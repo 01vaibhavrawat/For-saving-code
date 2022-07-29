87d9b7c818769105c8defe0dd541e125d9684b72
01vaibhavrawat <89971047+01vaibhavrawat@users.noreply.github.com>
2022-07-29 09:39:02 +0530

    Create leetcode.js

---
+++ leetcode.js
@@ -0,0 +1,10 @@
+var findAndReplacePattern = function(words, pattern) {
+    let array = [];
+    let first = words[0][0];
+    array[0] = ["a"];
+    if(words[0][1] != first){
+        array[1] = ["b"];
+    }
+    
+    console.log(array);
+};
